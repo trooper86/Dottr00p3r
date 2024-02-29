ea9d95b194b43490396560cc741dfb4841f949ad
trooper86 <128870400+trooper86@users.noreply.github.com>
2024-02-29 16:13:12 +1030

    Update config.fish

--- Dotfiles/config.fish
+++ Dotfiles/config.fish
@@ -169,7 +169,7 @@ alias cleanup='sudo pacman -Rns (pacman -Qtdq)'  # remove orphaned packages
 alias listorphans='sudo pacman -Qdt'             # lists orhpaned packages
 alias yaysyyu='yay -Syyu --noconfirm'            # update & sync standard pkgs and AUR
 alias yayss='yay -Ss'                            # Search for AUR packages
-
+alias yays='yay -S'                              # install form AUR 
 
 # get fastest mirrors
 alias mirror="sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist"
